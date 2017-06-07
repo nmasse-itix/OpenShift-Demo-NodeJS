cf8249acf93beefc75acad9abf9bd41d0f84976b
Nicolas Massé <nicolas.masse@itix.fr>
2017-06-07 17:37:32 +0200

    add unit tests. shhh !

---
+++ tests/run-unit-tests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Exit immediately if command returns non-zero status code
+set -e
+
+# Currently, it is just a syntax check
+node -c server.js
+
+
+# TODO: do a better job at unit testing :)
